de4aa5b5b2922a32bf2f64f229cf42dc92785f99
michaelresser <michaelresser@gmail.com>
2020-05-15 22:02:44 -0400

    React Router added.

--- src/components/Content/Exercises/Exercises/Exercises.js
+++ src/components/Content/Exercises/Exercises/Exercises.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Container, Fab, Modal } from '@material-ui/core';
+import React from 'react'
+import { Fab, Grid, Modal } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import AddIcon from '@material-ui/icons/Add';
 import { ExerciseList } from '../../../index';
@@ -9,61 +9,56 @@ import AppContext from '../../../Context/Context';
 const useStyles = makeStyles({
     root: {
         position: 'relative',
-        minHeight: '90vh',
+        height: '100%'
     },
     fabAdd: {
         position: 'absolute',
-        bottom: '8em',
+        bottom: '1em',
         right: '0',
         backgroundColor: 'green',
         '&:hover': {
             backgroundColor: 'lightGreen'
         },
     },
-    modalBody:{
+    modalBody: {
         color: 'white',
         position: 'relative',
         top: '50vh',
-        textAlign: 'center'        
+        textAlign: 'center'
     }
 })
 
-
-
 const Exercises = () => {
     const classes = useStyles();
-
     const [open, setOpen] = React.useState(false);
-
     const handleOpenModal = () => {
         setOpen(true);
     };
     const handleCloseModal = () => {
         setOpen(false);
     }
-
-
-
+    
     return (
         <AppContext.Consumer>
-            {context => (<Container className={classes.root}>
-
-                <ExerciseList />
-                <Fab
-                    color="primary"
-                    aria-label="add"
-                    className={classes.fabAdd}
-                    onClick={handleOpenModal}
-                >
-                    <AddIcon />
-                </Fab>
-                <Modal
-                    open={open}
-                    onClose={handleCloseModal}
-                >
-                    <h1 className={classes.modalBody}>Modal</h1>    
-                </Modal>
-            </Container>)}
+            {context => (
+                <Grid container className={classes.root}>
+                    <ExerciseList />
+                    <Fab
+                        color="primary"
+                        aria-label="add"
+                        className={classes.fabAdd}
+                        onClick={handleOpenModal}
+                    >
+                        <AddIcon />
+                    </Fab>
+                    <Modal
+                        open={open}
+                        onClose={handleCloseModal}
+                    >
+                        <h1 className={classes.modalBody}>Modal</h1>
+                    </Modal>
+                </Grid>
+            )}
         </AppContext.Consumer>
     )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,24 @@
 import App from './App/App';
+
+/* Layout Components */
 import Header from './Layout/Header/Header';
 import Footer from './Layout/Footer/Footer';
 import ViewPanel from './Layout/Views/ViewPanel/ViewPanel';
-
+// Home Section Components
+import Home from './Content/Home/Home';
+// Exercises Section Components
 import ExerciseTabs from './Layout/Views/ExerciseTabs/ExerciseTabs';
 import Exercises from './Content/Exercises/Exercises/Exercises';
 import ExerciseItem from './Content/Exercises/ExerciseItem/ExerciseItem';
 import ExerciseList from './Content/Exercises/ExerciseList/ExerciseList';
 
+/* Workouts Section Components */
+import Workouts from './Content/Workouts/Workouts/Workouts';
 
+/* Training Plans Sectio Components */
+import TrainingPlans from './Content/TrainingPlans/TrainingPlans.js';
 
-
-/* import Workouts from './Content/Exercises/Exercises/Exercises';
-import WorkoutList from './Content/Exercises/ExerciseList/ExerciseList';
-import WorkoutView from './Content/Exercises/ExerciseTable/ExerciseTable'; */
-
+/* Context */
 import AppContext, {AppState} from './Context/Context';
 
 export {
@@ -23,13 +27,12 @@ export {
     AppState,
     Header,
     ViewPanel,
+    Home,
     ExerciseTabs,
     Exercises,
     ExerciseItem,
     ExerciseList,
- /*    Workouts,
-    WorkoutActions,
-    WorkoutList,
-    WorkoutView, */
+    Workouts,
+    TrainingPlans,
     Footer
 }
